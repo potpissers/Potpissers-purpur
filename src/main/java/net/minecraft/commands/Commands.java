89f7c65e8726f9f51355594e86eb7c68adf6b58b
Encode42 <me@encode42.dev>
2021-04-30 14:03:06 -0400

    Add credits command

--- src/main/java/net/minecraft/commands/Commands.java
+++ src/main/java/net/minecraft/commands/Commands.java
@@ -250,6 +250,7 @@ public class Commands {
             StopCommand.register(this.dispatcher);
             TransferCommand.register(this.dispatcher);
             WhitelistCommand.register(this.dispatcher);
+            org.purpurmc.purpur.command.CreditsCommand.register(this.dispatcher); // Purpur
             org.purpurmc.purpur.command.DemoCommand.register(this.dispatcher); // Purpur
             org.purpurmc.purpur.command.PingCommand.register(this.dispatcher); // Purpur
         }
