pvginkel/Tweakkit-Server

Safer JSON Loading

src/main/java/net/minecraft/server/UserCache.java
@@ -176,6 +176,11 @@ public class UserCache {
                 break label81;
             } catch (FileNotFoundException filenotfoundexception) {
                 ;
+            // Spigot Start
+            } catch (net.minecraft.util.com.google.gson.JsonSyntaxException ex) {
+                JsonList.a.warn( "Usercache.json is corrupted or has bad formatting. Deleting it to prevent further issues." );
+                this.g.delete();
+            // Spigot End
             } finally {
                 IOUtils.closeQuietly(bufferedreader);
             }

potpissers/Potpissers-purpur

src/main/java/net/minecraft/server/players/UserCache.java
@@ -250,6 +250,11 @@ public class UserCache {
             }
         } catch (FileNotFoundException filenotfoundexception) {
             ;
+        // Spigot Start
+        } catch (com.google.gson.JsonSyntaxException | NullPointerException ex) {
+            UserCache.LOGGER.warn( "Usercache.json is corrupted or has bad formatting. Deleting it to prevent further issues." );
+            this.file.delete();
+        // Spigot End
         } catch (JsonParseException | IOException ioexception) {
             UserCache.LOGGER.warn("Failed to load profile cache {}", this.file, ioexception);
         }